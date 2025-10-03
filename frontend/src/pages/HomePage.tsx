b31b1f1cc0b11d46dfa2e98a786b76c9cf0e1ad4
wudye <mingwei.wu@hotmail.com>
2025-10-03 18:02:22 +0200

    create frontend

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Chatroom</h1>
+      <p>Select a chatroom to join:</p>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ frontend/src/pages/UserPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserPage = () => {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>View and edit your profile information here.</p>
+    </div>
+  )
+}
+
+export default UserPage
